7d4b28365c125642056dfd805c025f6acc343be2
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-07-16 23:34:08 -0700

    added the Rect header file

--- ShapeProject/ShapeProject/Rect.h
+++ ShapeProject/ShapeProject/Rect.h
@@ -1,9 +1,38 @@
 #ifndef RECT_H
 #define RECT_H
+#include "Shape.h"
 
-class Rect{
+class Rect {
+	//two private variables which will be accessed by the public functions 
+	//this is using the encapsulation property of OOP.
 private:
 	double length, width;
+public:
+	//constructor , assigning parameter values to member variables
+	Rect(double a, double b){
+		length = a;
+		width = b;
+
+	}
+	//set functions for changing the values of length and width
+	void setLength(double a){
+		length = a;
+	}
+	void setWidth(double  b){
+		width = b;
+	}
+	//get functiions to return length and width of rectangle
+	double getLength(){
+		return length;
+	}
+	double getWidth(){
+		return width;
+	}
+
+	double getArea(){
+		return length*width;
+
+	}
 
 };
 #endif
