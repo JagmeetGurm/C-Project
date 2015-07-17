1a22ddb5578f4d491acf09428f5d911c44f86dc9
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-07-16 23:11:16 -0700

    added the comments for the Shape class

---
+++ ShapeProject/ShapeProject/Rect.h
@@ -0,0 +1,9 @@
+#ifndef RECT_H
+#define RECT_H
+
+class Rect{
+private:
+	double length, width;
+
+};
+#endif
--- ShapeProject/ShapeProject/Shape.h
+++ ShapeProject/ShapeProject/Shape.h
@@ -1,7 +1,10 @@
 #ifndef SHAPE_H
 #define SHAPE_H
 
+//abstract class shape
 class Shape{
+
+	//two public functions which will be overwritten by the inherited classes
 public:
 bool	checkValidity();
 double getArea();
