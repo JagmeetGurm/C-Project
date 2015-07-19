ac6321677b75f0b213fe90501fe270455adac1dc
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-07-19 13:10:59 -0700

    added header file with a class of diff types of functions

--- Overloading/Overloading/PrintData.h
+++ Overloading/Overloading/PrintData.h
@@ -6,13 +6,16 @@ using namespace std;
 class PrintData{
 public:
 
+	//function to print integer value
 	void print(int i)
 	{
 		cout << i << endl;
 	}
+	//function to print float value
 	void print(float i){
 		cout << i << endl;
 	}
+	//function to print characters
 	void print(char* c){
 		cout << c << endl;
 	}
