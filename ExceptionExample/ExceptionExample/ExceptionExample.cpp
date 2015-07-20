bbe6a67f917adccafcd2cee2e509440b214ec460
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-07-20 16:24:48 -0700

    Dynamic memory executed inside it

--- ExceptionExample/ExceptionExample/ExceptionExample.cpp
+++ ExceptionExample/ExceptionExample/ExceptionExample.cpp
@@ -24,6 +24,15 @@ int _tmain(int argc, _TCHAR* argv[])
 	catch (const char* a){
 		cerr << a << endl;
 	}
+	double* pvalue = NULL; // Pointer initialized with null
+	pvalue = new double;   // Request memory for the variable
+
+	*pvalue = 29494.99;     // Store value at allocated address
+	cout << "Value of pvalue : " << *pvalue << endl;
+
+	delete pvalue;         // free up the memory.
+
+	
 	system("pause");
 	return 0;
 }
