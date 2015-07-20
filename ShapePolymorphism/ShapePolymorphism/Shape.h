bf1a0b256d08a0fceb123850709dc063e68447ad
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-07-19 21:56:16 -0700

    Shape class added

---
+++ ShapePolymorphism/ShapePolymorphism/Shape.h
@@ -0,0 +1,15 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+#include <iostream>
+
+using namespace std;
+
+class Shape{
+private:
+	int length, width;
+public:
+	int getArea(){
+		cout << "the area is: ";
+	}
+};
+#endif
