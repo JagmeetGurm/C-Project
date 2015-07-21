0fe4b3b480ae9e64518f282fb619a8f8676e862e
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-07-20 17:55:52 -0700

    function template example

--- TemplateExample/TemplateExample/TemplateExample.cpp
+++ TemplateExample/TemplateExample/TemplateExample.cpp
@@ -2,10 +2,22 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
+
+template <typename T>
+T Max(T a, T b){
+	return(a > b ? a : b);
+
+}
 
 int main()
 {
+	cout<<"int max: "<<Max(3, 5)<<endl;
+	cout << "double max: " << Max(4.5, 9.8) << endl;
+	cout << "string max: " << Max("jim", "abcd")<<endl;
+	system("pause");
 	return 0;
 }
 
