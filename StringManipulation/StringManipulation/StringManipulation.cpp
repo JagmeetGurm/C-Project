c507ecfff910472440a11f17721e079961a3145b
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-07-30 21:45:34 -0700

    finding length of string not using length method

--- StringManipulation/StringManipulation/StringManipulation.cpp
+++ StringManipulation/StringManipulation/StringManipulation.cpp
@@ -9,12 +9,25 @@ using namespace std;
 int lengthString(string myString){
 	return myString.length();
 
+	
+}
+//function to count length of string without using length method
+int lengthString2(string myString2){
+	//char c=myString2[0];
+	int i = 0;
+	while (myString2[i] != '\0'){
+		i++;
+
+	}
+	return i;
+
 }
 int main()
 {
 	string s;
 	s = "jim is great";
 cout<<"the length of string is: "<<	lengthString(s)<<endl;
+cout << "lenght of string using 2nd way, not using the length method: " << lengthString2(s) << endl;
 
 system("pause");
 	return 0;
