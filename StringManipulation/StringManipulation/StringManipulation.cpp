8bfe48ab13d8821079acf01ee0d7878a3afc9591
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-07-30 20:58:47 -0700

    finding length of string using length method

--- StringManipulation/StringManipulation/StringManipulation.cpp
+++ StringManipulation/StringManipulation/StringManipulation.cpp
@@ -2,10 +2,21 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+//function to find the length of the string using length method of string
+int lengthString(string myString){
+	return myString.length();
 
-
-int _tmain(int argc, _TCHAR* argv[])
+}
+int main()
 {
+	string s;
+	s = "jim is great";
+cout<<"the length of string is: "<<	lengthString(s)<<endl;
+
+system("pause");
 	return 0;
 }
 
