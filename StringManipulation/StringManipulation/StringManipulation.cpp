81beb70ce9f92b6e580ba3fd8d10cf181b94cb1e
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-08-04 17:50:39 -0700

    added another test case

--- StringManipulation/StringManipulation/StringManipulation.cpp
+++ StringManipulation/StringManipulation/StringManipulation.cpp
@@ -264,6 +264,7 @@ cout<<endl;
 cout << "is anagram: " << isAnagram("army", "mary");//simple test case
 cout << "is anagram: " << isAnagram("army", "marsy"); //test case
 cout << "is anagram: " << isAnagram("jim", "img");
+cout << "is: " << isAnagram("abc", "abcd");
 //cout << "is anagram: " << isAnagram("mummy", "ummyt");
 system("pause");
 	return 0;
