8a7b4a2e2f03be55b1524ca03e3a806ad27d2df5
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-08-04 17:53:34 -0700

    added comments

--- StringManipulation/StringManipulation/StringManipulation.cpp
+++ StringManipulation/StringManipulation/StringManipulation.cpp
@@ -193,6 +193,8 @@ void printDuplicateChar2(string s){
 	
 }
 //function to check if 2 strings are anagrams
+//doesn't work for all cases
+//will look into it later on
 bool isAnagram(string s1, string s2)
 {
 	int len1 = s1.length();
