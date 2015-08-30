1d6128fc93f3484b8c9a47298d3a92bf7ce74407
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-08-30 16:43:45 -0700

    declaring two arrays

--- BeautyStrings/BeautyStrings/BeautyStrings.cpp
+++ BeautyStrings/BeautyStrings/BeautyStrings.cpp
@@ -28,7 +28,8 @@ using namespace std;
 
 int main()
 {
-	string a[10];
+	char letter[26]; //array to store unique letters
+	int frequency[26]; //array to store frequency for each letter
 	string input;
 	ifstream inputFile;
 	vector<string> v;
