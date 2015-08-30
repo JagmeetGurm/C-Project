16f92c3a4d0a1aeec2b665412e058bc73f82cc62
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-08-29 22:39:30 -0700

    added comments regarding project

--- BeautyStrings/BeautyStrings/BeautyStrings.cpp
+++ BeautyStrings/BeautyStrings/BeautyStrings.cpp
@@ -1,5 +1,8 @@
 // BeautyStrings.cpp : Defines the entry point for the console application.
 //
+//Beauty Strings Project to calculate the maximum baeauty of the string. 
+//Each character will be assigned a beauty value between 1 and 26 (including) 
+//and the sum of the chars will be the beauty of the string
 
 #include "stdafx.h"
 
