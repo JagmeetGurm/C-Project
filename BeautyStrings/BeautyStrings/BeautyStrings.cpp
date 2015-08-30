e2cd66b333396005c2538975a12c052a7b6d6bc1
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-08-29 22:47:38 -0700

    added comments regarding methodology

--- BeautyStrings/BeautyStrings/BeautyStrings.cpp
+++ BeautyStrings/BeautyStrings/BeautyStrings.cpp
@@ -4,6 +4,11 @@
 //Each character will be assigned a beauty value between 1 and 26 (including) 
 //and the sum of the chars will be the beauty of the string
 
+
+//Methodology 
+//First we need to accept filename as an argument.
+//Then, We need a way to calculate each strings maximum beauty
+
 #include "stdafx.h"
 
 
