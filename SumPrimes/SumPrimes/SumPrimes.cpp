5522c71fef63bbc86970bb58123cf8e84d855d1a
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-09-04 17:54:34 -0700

    Sum of first 1000 Primes

---
+++ SumPrimes/SumPrimes/SumPrimes.cpp
@@ -0,0 +1,22 @@
+// SumPrimes.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int sum[1000];
+	sum[0] = 2;
+	bool t = true;
+	while (t){
+		for (int i = 3;; i = i + 2){
+
+		}
+	}
+	return 0;
+}
+
