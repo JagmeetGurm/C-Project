24e74261f22e441e07d6b13fac7782d5b4184d39
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-09-04 18:45:06 -0700

    inner loop added to check for prime

--- SumPrimes/SumPrimes/SumPrimes.cpp
+++ SumPrimes/SumPrimes/SumPrimes.cpp
@@ -13,14 +13,29 @@ int main()
 	int sum; //int variable to hold sum of primes 
 	sum = 2;
 	bool t = true;
+	int count = 1;
 	while (t){
 		for (int i = 3;; i = i + 2){ // outer loop to go through all possible first primes
-			for (int j = 2; j <= (i / 2); j++){
-				//if (i%j)
+			int j;
+			for (j = 2; j <= (i / 2); j++){ //inner loop to check if no is prime
+				if (i%j == 0)
+					j = (i / 2)+1;
+				else continue;
+
+			}
+			if (j > (i / 2)){//that means no is prime
+
+				sum += i;  //add prime no to total
+				count++;
+			}
+			if (count == 1000){
+				t = false;
 			}
 
 		}
 	}
+	cout << "total: " << sum;
+	system("pause");
 	return 0;
 }
 
