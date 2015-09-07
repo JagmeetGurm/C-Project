eb37ca45a7a0d28b931ed1093c4bc79d6d8e4796
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-09-06 22:46:13 -0700

    reading from file

--- FibonacciSeries/FibonacciSeries/FibonacciSeries.cpp
+++ FibonacciSeries/FibonacciSeries/FibonacciSeries.cpp
@@ -2,10 +2,22 @@
 //
 
 #include "stdafx.h"
+#include <iosream>
+#include <fstream>
+
+using namespace std;
+//first will read numbers from file line by line and 
+//then print corresponding fibonacci number
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	ifstream inputFile;
+	inputFile.open("Fibonacci.txt");
+	int a;
+	while (inputFile >> a){
+
+	}
 	return 0;
 }
 
