0b26f087983d017b4d807932c5fb715bb94f4678
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-09-06 22:52:16 -0700

    recursive fibo function added

--- FibonacciSeries/FibonacciSeries/FibonacciSeries.cpp
+++ FibonacciSeries/FibonacciSeries/FibonacciSeries.cpp
@@ -2,27 +2,33 @@
 //
 
 #include "stdafx.h"
-#include <iosream>
+#include <iostream>
 #include <fstream>
 
-using namespace std;
+//using namespace std;
 //first will read numbers from file line by line and 
 //then print corresponding fibonacci number
 
 
 int fibo(int n){
-
+	if (n == 0)
+		return 0;
+	else if (n == 1)
+		return 1;
+	else return fibo(n - 1) + fibo(n - 2);
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	ifstream inputFile;
+	std::ifstream inputFile;
 	inputFile.open("Fibonacci.txt");
 	int a;
 	while (inputFile >> a){
-		fibo(a);
+	
+		
 
 	}
+	system("pause");
 	return 0;
 }
 
