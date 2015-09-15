df6f995e617a35e6e29d8d467146a0e9e17cf3e9
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-09-15 12:21:27 -0700

    assigning pointers to points

--- BayBridges/BayBridges/BayBridges.cpp
+++ BayBridges/BayBridges/BayBridges.cpp
@@ -27,21 +27,36 @@ public:
 private:
 	double x, y;
 };
-class lineSegment{
+class lineSegment {
 public:
 	//line segment format: l1(x1, y1, x2, y2)
 	//lineSegment(point A(double a1, double b1), point B(double a2, double b2)){
-	lineSegment(point, point){
-
+	lineSegment(point a, point b){
+		z1 = new point(a.X(), a.Y()); //creating a new point to which z1 points
+		z2 = new point(b.X(), b.Y());
+	//	cout << "print pointer: ";
+	//	cout << z1->Y();
+		//z2 = b;
+		//B = b;
 	}
 	void aX(point a){
+		//z1 = a;
 		cout<< a.X();
 	}
 	//void a(double a)
 	void aY(point a){
 		cout<<a.Y();
 	}
-	
+	point* ptA(){
+		return z1;
+	}
+	point* ptB(){
+		return z2;
+	}
+
+private:
+	point* z1;
+	point* z2;
 
 };
 
@@ -55,7 +70,13 @@ int main(int argc, _TCHAR* argv[])
 	cout << A.Y()<<endl;
 	lineSegment l1(A, B);
 	cout << "lets see: ";
-	 l1.aX(A);
+	 l1.aX(A); //this should print the x coordinate of point passed to line segment
+	 cout << "now this should print y coordinate of point A: ";
+	 cout<< l1.ptA()->Y();
+	 cout << "print y coordinate of b: ";
+	 cout << l1.ptB()->Y();
+	//cout<< p.Y();
+//	cout<< p.Y();
 	system("pause");
 	return 0;
 }
