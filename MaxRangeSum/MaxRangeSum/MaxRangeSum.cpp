535e16f127784cb6bc9f0ef2d9d7bd9fa2dfcab4
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-09-24 15:39:46 -0700

    pushing values for a line into vector

--- MaxRangeSum/MaxRangeSum/MaxRangeSum.cpp
+++ MaxRangeSum/MaxRangeSum/MaxRangeSum.cpp
@@ -21,21 +21,41 @@ int main(int argc, char* argv[])
 	infile.open("inputSum.txt");
 	string s;
 	while (getline(infile, s)){
-
+		vector<int>profit;
 		int i = 0; 
 		int j = 0;
 		while (s[i] != ';'){
 			j++;
 			i++;
-		}		
+		}	
+	
 		string s1 = s.substr(0, j);
 		//cout << s1<<endl;
 		int day = stoi(s1);
 		//cout << day << endl;
 			numDays.push_back(day);
-			cout << numDays[i]+10<<endl;
-		
+			int z = j+1;
+		//	cout << s[j + 1];
+			while (z  < s.length()){
+				int k = 0;
+				j = z;
+				while (z<s.length() && s[z] != ' '){
+					k++;
+					z++;
+				}
+				string s2 = s.substr(j , k);
+				int value = stoi(s2);
+				profit.push_back(value);
+				//cout << value +100 << endl;
+				
+				z++;
+
+			}
+			
+			
 	}
+	
+	
 	system("pause");
 	return 0;
 }
