93896dd9defe36ab3145092f9220f627af4ceb62
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-09-24 12:13:59 -0700

    program to calculate maximum sum range

---
+++ MaxRangeSum/MaxRangeSum/MaxRangeSum.cpp
@@ -0,0 +1,14 @@
+// MaxRangeSum.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	system("pause");
+	return 0;
+}
+
