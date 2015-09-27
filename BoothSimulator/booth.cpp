464e024e8e37aad66d42ede9ed8238d5e3fd1496
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-09-27 14:50:47 -0700

    added the input standard format

--- BoothSimulator/booth.cpp
+++ BoothSimulator/booth.cpp
@@ -7,10 +7,21 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-int a=0b1;
-int b=0b1;
-int c=a+b;
-bitset<2> x(c);
-cout<<x;
+int ai=0b1;
+int bi=0b1;
+//int c=a+b;
+int carryIn=0b0;
+int op=0b0;
+//entering the four inputs
+cout<<"Enter the ai: ";
+cin>>ai;
+cout<<"\n Enter the bi: ";
+cin>>bi;
+cout<<"\n Enter the carryin: ";
+cin>>carryIn;
+cout<<"Enter the op signal: ";
+cin>>op;
+//bitset<2> x(c);
+//cout<<x;
 	return 0;
 }
