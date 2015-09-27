a563dc13bfc5d18759813556991f951d70fc3dc2
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-09-27 15:33:57 -0700

    added the adder

--- BoothSimulator/booth.cpp
+++ BoothSimulator/booth.cpp
@@ -3,24 +3,40 @@
 
 using namespace std;
 
+//global variables result and carryOut, sum, carryIn, op;
+int result=0b0, carryOut=0b0;
+int sum=0b0;
+int carryIn=0b0;
+int op=0b0;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void adder(int a, int b){
+	 sum=a+b;
+	bitset<1> result(sum);
+	
+	cout<<result;
+	
+}
 
+void
 int main(int argc, char** argv) {
 int ai=0b1;
 int bi=0b1;
 //int c=a+b;
-int carryIn=0b0;
-int op=0b0;
+
 //entering the four inputs
 cout<<"Enter the ai: ";
 cin>>ai;
-cout<<"\n Enter the bi: ";
+cout<<"Enter the bi: ";
 cin>>bi;
-cout<<"\n Enter the carryin: ";
+cout<<"Enter the carryin: ";
 cin>>carryIn;
 cout<<"Enter the op signal: ";
 cin>>op;
+adder(ai, bi);
+bitset<1>result(sum);
+cout<<"result: "<<result<<endl;
+cout<<"carryout: "<<carryOut<<endl;
 //bitset<2> x(c);
 //cout<<x;
 	return 0;
