fb9218626a780c942afa33c877eab2fc709863c9
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-09-27 18:42:45 -0700

    alu function called

--- BoothSimulator/booth.cpp
+++ BoothSimulator/booth.cpp
@@ -4,35 +4,37 @@
 using namespace std;
 
 //global variables result and carryOut, sum, carryIn, op;
-int result=0b0, carryOut=0b0;
+bitset<1> result=0b0, carryOut=0b0;
 int sum=0b0;
 int carryIn=0b0;
 int op=0b0;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int adder(int a, int b){
+bitset<1> adder(int a, int b){
 	int sum1=a+b;
 	bitset<1> result(sum1);
 	if(a==1 && b==1)
 	carryOut=1;
 	//cout<<result;
-return sum1;	
+return result;	
 }
 
 int andGate(int a, int b){
+	int sum2;
 	if(a==1 && b==1)
-	int sum2=1;
+	sum2=1;
 	else sum2=0;
 	return sum2;
 }
 
 int orGate(int a, int b){
+	int sum3;
 	if(a==0 && b==0)
-	int sum3=0;
+	 sum3=0;
 	else sum3=1;
 	return sum3;
 }
-void mux(int andResult, int orResult, int addResult, int opSignal){
+bitset<1> mux(int andResult, int orResult, bitset<1> addResult, int opSignal){
 	if(opSignal==0)
 	return addResult;
 	if(opSignal==01)
@@ -42,10 +44,10 @@ void mux(int andResult, int orResult, int addResult, int opSignal){
 	 
 	
 }
-int alu(int a, int b, int c, int d){
+bitset<1> alu(int a, int b, int c, int d){
 	int andResult=andGate(a, b);
 	int orResult=orGate(a, b);
-	int addResult=adder(a, b);
+	bitset<1> addResult=adder(a, b);
 result=	mux(andResult, orResult, addResult, d);
 return result;
 }
@@ -66,10 +68,18 @@ cout<<"Enter the op signal: ";
 cin>>op;
 //adder(ai, bi);
 //andGate(ai, bi);
-orGate(ai, bi);
-bitset<1>result(sum);
-cout<<"result: "<<result<<endl;
+//orGate(ai, bi);
+//bitset<1>result(sum);
+if(op==11)
+{if(bi==0)
+bi=1;
+else bi=0;
+}
+cout<<"result: "<<alu(ai, bi, carryIn, op);
+cout<<endl;
+if(op==10)
 cout<<"carryout: "<<carryOut<<endl;
+else cout<<"carryout: "<<0<<endl;
 //bitset<2> x(c);
 //cout<<x;
 	return 0;
