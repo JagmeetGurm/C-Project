a113dc9c465aa8b07992df13ce92ca53992b7129
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-12-01 12:50:07 -0800

    storing strings after reading from file in vector

--- StringPermutation/StringPermutation/StringPermutation.cpp
+++ StringPermutation/StringPermutation/StringPermutation.cpp
@@ -6,18 +6,24 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <vector>
+
 
 using namespace std;
 
 
 int main(int argc, _TCHAR* argv[])
 {
+	vector<string> strings;
 	string word;
 	ifstream inputFile;
 	inputFile.open("stringInput.txt");
 	while (getline(inputFile, word)){
-		cout << word << endl;
+	//	cout << word << endl;
+		//storing words in string vector
+		strings.push_back(word);
 	}
+	//calling permute function
 	system("pause");
 	return 0;
 }
