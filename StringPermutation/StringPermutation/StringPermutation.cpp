6d77e968815fdf95dfd0a0923d2d1a9471950dda
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-12-02 00:34:11 -0800

    sorting the restult

--- StringPermutation/StringPermutation/StringPermutation.cpp
+++ StringPermutation/StringPermutation/StringPermutation.cpp
@@ -7,11 +7,14 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <algorithm>
 
 
 using namespace std;
 //swap function to swap chars in a string
 int numOfPermutations = 0;
+int times = 0;
+vector<string>order;
 int factorial(int n){
 	if (n == 0|| n==1)
 		return 1;
@@ -27,15 +30,29 @@ void swap(char& a, char& b){
 void permute(string& s, int i, int n)
 {
 	
-	if (i == n) cout << s;
-	//if(k!=n)
-		//cout<< ",";
+	if (i == n){ 
+		//cout << s;
+		order.push_back(s);
+	//	times++;
+	//if (times < numOfPermutations)
+	
+		// cout << ",";
+	}
 	else{
 		for ( int k = i; k <= n; k++){
 			swap(s[i], s[k]);
 			permute(s, i + 1, n);
 			swap(s[i], s[k]);
 		}
+	}
+}
+void print(vector<string>vs){
+	for (int i = 0; i<vs.size(); i++){
+		cout << vs[i];
+		times++;
+		if (times < numOfPermutations)
+
+			cout << ",";
 	}
 }
 int main(int argc, _TCHAR* argv[])
@@ -50,13 +67,20 @@ int main(int argc, _TCHAR* argv[])
 		strings.push_back(word);
 	}
 	//calling permute function
-/*	for (int i = 0; i < strings.size(); i++){
+	for (int i = 0; i < strings.size(); i++){
+		//numOfPermutations = factorial(strings[i].length());
 		permute(strings[i], 0, strings[i].length()-1);
+		numOfPermutations = factorial(strings[i].length());
+	std::sort(order.begin(), order.end());
+		print(order);
+		vector<string> temp;
+		order = temp;
 		cout << endl;
-	}*/
-	numOfPermutations = factorial(3);
+		times = 0;
+	}
 	
-	permute(strings[0], 0, 3 - 1);
+	
+	//permute(strings[0], 0, 3 - 1);
 	system("pause");
 	return 0;
 }
