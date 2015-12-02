0f3a6886666b5da868d43f3198fdd1d616d7bb62
JagmeetGurm <jagmeetgurm@yahoo.com>
2015-12-01 23:14:51 -0800

    performing the permutation

--- StringPermutation/StringPermutation/StringPermutation.cpp
+++ StringPermutation/StringPermutation/StringPermutation.cpp
@@ -10,8 +10,34 @@
 
 
 using namespace std;
+//swap function to swap chars in a string
+int numOfPermutations = 0;
+int factorial(int n){
+	if (n == 0|| n==1)
+		return 1;
+	else return n*(n - 1);
 
-
+}
+void swap(char& a, char& b){
+	char temp=a;
+	a = b;
+	b = temp;
+}
+//permute function
+void permute(string& s, int i, int n)
+{
+	
+	if (i == n) cout << s;
+	//if(k!=n)
+		//cout<< ",";
+	else{
+		for ( int k = i; k <= n; k++){
+			swap(s[i], s[k]);
+			permute(s, i + 1, n);
+			swap(s[i], s[k]);
+		}
+	}
+}
 int main(int argc, _TCHAR* argv[])
 {
 	vector<string> strings;
@@ -24,6 +50,13 @@ int main(int argc, _TCHAR* argv[])
 		strings.push_back(word);
 	}
 	//calling permute function
+/*	for (int i = 0; i < strings.size(); i++){
+		permute(strings[i], 0, strings[i].length()-1);
+		cout << endl;
+	}*/
+	numOfPermutations = factorial(3);
+	
+	permute(strings[0], 0, 3 - 1);
 	system("pause");
 	return 0;
 }
