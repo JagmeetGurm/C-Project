2863384d97afa3e92b6074f90843db356e396adc
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-02-13 23:47:22 -0800

    using print seq for printing order

--- EightPuzzle3/EightPuzzle3/puzzle.h
+++ EightPuzzle3/EightPuzzle3/puzzle.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <queue>
 #include "node.h"
 using namespace std;
 
@@ -13,8 +14,46 @@ public:
 	puzzle(string iState, string gState){
 		initialState = iState;
 		goalState = gState;
-		count = 0;
+		//count = 0;
 
+
+	}
+	//void move()
+	void move(){
+		node* nn;
+		nn = new node;
+		nn->state = initialState;
+		nn->parent = NULL;
+		nn->levelDepth = 0;
+		root = nn;
+		q.push(nn);
+		while (!q.empty()){
+			current = q.pop();
+			
+			if (current->state == goalState){
+				cout << "found at level or after moves: " << current->levelDepth<<endl;
+				printSequence();
+			}
+			//left
+			else if (current->state.(0) != '0' && current->state.(3) != '0' && current->state.(6) != '0'){
+				string temp = current->state;
+				current-
+			}
+		}
+
+	}
+	void printSequence(){
+		vector<string>seq;
+		vector<string>dir;
+		while (current != NULL){
+			seq.push_back(current->state);
+			dir.push_back(current->direction);
+			current = current->parent;
+		}
+		for (int i = seq.size() - 1; i >= 0; i++){
+			cout << seq[i]<<endl;
+			cout << dir[i] << endl;
+		}
 	}
 
 private:
@@ -22,6 +61,8 @@ private:
 	node* current;
 	node* root;
 	int count;
+
+	queue<puzzle>q;
 };
 
 
