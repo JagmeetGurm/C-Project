3231172daa99649a8444aa812825cbb44241f188
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-02-28 22:30:09 -0800

    func generateComb to generate all combs

--- MotifFinding/MotifFinding/MotifFinding.cpp
+++ MotifFinding/MotifFinding/MotifFinding.cpp
@@ -10,6 +10,32 @@
 
 using namespace std;
 
+void generateComb(vector<string>& comb){
+	char	t[4] = { 'A', 'C', 'G', 'T' };
+//	string t = "ACGT";
+	string temp;
+	for (int a = 0; a < 6; a++){
+		for (int b = 0; b < 6; b++){
+			for (int c = 0; c < 6; c++){
+				for (int d = 0; d < 6; d++){
+					for (int e = 0; e < 6; e++){
+						for (int f = 0; f < 6; f++){
+							 temp =t[a]+ t[b] + t[c] + t[d] + t[e] + t[f];
+							comb.push_back(temp);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+void medianSearch(unordered_map<string, string>& dna, vector<string>& labels, int l){
+	string bestWord = "AAAAAA";
+	int bestDistance = 5000;
+	vector<string>combinations;
+	generateComb(combinations);
+
+}
 int main()
 {
 
@@ -60,6 +86,8 @@ int main()
 
 		cout << "label: " << labels[2] << endl;
 		cout << "seq: " << dna[labels[2]] << endl;
+
+		medianSearch(dna, labels, 6);
 	}
 		system("pause");
 	return 0;
