3baccc394f36e5e05c95c789e211d35649868c53
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-04-27 17:10:20 -0700

    RNA sequence pipelining program

---
+++ RNApipeline/RNApipeline/RNApipeline.cpp
@@ -0,0 +1,20 @@
+// RNApipeline.cpp : Defines the entry point for the console application.
+//RNA sequence pipelining
+
+
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+
+using namespace std;
+
+int main()
+{
+
+	return 0;
+}
+
