2a0150be5338c8f7aa3e3a644efef7d375aaf2d0
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-04-27 22:59:09 -0700

    file parsed and genes stored in vector

--- RNApipeline/RNApipeline/RNApipeline.cpp
+++ RNApipeline/RNApipeline/RNApipeline.cpp
@@ -8,18 +8,23 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <sstream>
+#include <iterator>
+
 
 
 using namespace std;
 
 class gene{
+	
+
 public:
-	gene(int src, int dest, int dummy, string id, char strand){
+	gene(int src, int dest, string id){
 		this->src = src;
 		this->dest = dest;
-		this->dummy = dummy;
+		//this->dummy = dummy;
 		geneID = id;
-		this->strand = strand;
+		//this->strand = strand;
 
 
 	}
@@ -36,13 +41,16 @@ public:
 private:
 	int src, dest, dummy;
 	string geneID;
-	char strand;
+	string strand;
 
 };
 int main()
 {
+	
+
 	ifstream inputFile;
 	string line;
+	vector<gene>geneStorage;
 	inputFile.open("HG19-refseq-exon-annot-chr1-2016");
 	if (!inputFile){
 		cout << "sorry! can't open the file" << endl;
@@ -52,12 +60,27 @@ int main()
 		while (!inputFile.eof()){
 			getline(inputFile, line);
 			//check the file being read
-			cout << line << endl;
-//
+		//	cout << line << endl;
+			//parsing the string line
+			std::istringstream buf(line);
+			std::istream_iterator<std::string> beg(buf), end;
 
+			std::vector<std::string> tokens(beg, end); // done!
+		//	cout << tokens[2]<<endl;
+	//		for (auto& s : tokens)
+		//		std::cout << '"' << s << '"' << '\n';
+//lets create the genes;
+			std::string::size_type sz;
+			
+			gene g(stoi(tokens[1]), stoi(tokens[2]), tokens[3]);
+			geneStorage.push_back(g);
+		//	cout << geneStorage.size() << endl;
 		}
 	}
-
+	cout<<geneStorage[0].sourceID()<<endl;
+	cout << geneStorage[1].destID() << endl;
+	cout << geneStorage[2].gID() << endl;
+	system("pause");
 	return 0;
 }
 
