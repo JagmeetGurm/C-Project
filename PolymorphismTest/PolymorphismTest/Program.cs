4157639eed6f1aab362b31e4ca0137b3e3d87987
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-06-08 16:36:33 -0700

    poly project

---
+++ PolymorphismTest/PolymorphismTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismTest
+{
+
+   public class Employee
+    {
+        public void CalculateWeeklySalary(int hours, int wage)
+        {
+            var salary = 40 * wage;
+            Console.WriteLine("This employee is angry because he worked for {0} hrs." +
+                "But got paid for 40 hrs at {1}/hrs={2} salary.", hours, wage, salary);
+            Console.WriteLine("----------");
+
+        }
+    }
+    public class Contractor: 
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
