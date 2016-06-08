ee91d31b90ffe60ac92569f2c46250d6404af69b
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-06-08 12:02:09 -0700

    test failed

---
+++ SalaryCalculationTestProject/SalaryCalculator/SalaryCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class SalaryCalculator
+    {public decimal GetAnnualSalary(decimal hourlyWage)
+        {
+            const int hoursInYear = 2080;
+            return 0;
+
+        }
+    }
+}
