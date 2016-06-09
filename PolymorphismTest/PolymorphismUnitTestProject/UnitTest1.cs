89dbf29e9b5a92c310dbe98fc4e2a71572cf987f
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-06-08 20:25:17 -0700

    poly project-test case passed

--- PolymorphismTest/PolymorphismUnitTestProject/UnitTest1.cs
+++ PolymorphismTest/PolymorphismUnitTestProject/UnitTest1.cs
@@ -13,12 +13,30 @@ namespace PolymorphismUnitTestProject
             //arrage
             int hours = 55;
             int wage = 70;
-            int salary = 1;
+            int salary = 40*wage;
             Employee e = new Employee();
             string expectedResponse= String.Format("This employee is angry because he worked for {0} hrs." +
                 "But got paid for 40 hrs at {1}/hrs=$ {2} salary.", hours, wage, salary);
             //act
             string response = e.CalculateWeeklySalary(hours, wage);
+            //assert
+            Assert.AreEqual(response, expectedResponse);
+        }
+
+
+        [TestMethod]
+        public void SalaryContractorTest()
+        {
+            //arrage
+            int hours = 55;
+            int wage = 70;
+            int salary = hours*wage;
+            Contractor c= new Contractor();
+            string expectedResponse = String.Format("\nThis HAPPY CONTRACTOR worked {0} hrs. " +
+                              "Paid for {0} hrs at $ {1}" +
+                              "/hr = ${2} ", hours, wage, salary);
+            //act
+            string response = c.CalculateWeeklySalary(hours, wage);
             //assert
             Assert.AreEqual(response, expectedResponse);
         }
