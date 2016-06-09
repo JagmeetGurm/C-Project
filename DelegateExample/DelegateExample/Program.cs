29e5ca2029b1ddd6de59ac9fbd70537792837028
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-06-09 16:33:00 -0700

    delegates example

---
+++ DelegateExample/DelegateExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateExample
+{
+    public delegate string MyDelegate(int a, int b);
+    class Program
+    {
+        static string add(int a, int b)
+        {
+            return (a + b).ToString();
+
+        }
+        static string prod(int a, int b)
+        {
+            return (a * b).ToString();
+        }
+        static void Main(string[] args)
+        {
+
+
+            MyDelegate m = add;
+          Console.WriteLine( $"First call the add via delegate: {m(4,5) }");
+            Console.ReadKey();
+        }
+    }
+}
