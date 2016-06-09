16fb1bc569072856dda1bfab5eaa3e0af2e26024
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-06-08 17:31:43 -0700

    poly project

--- PolymorphismTest/PolymorphismTest/Program.cs
+++ PolymorphismTest/PolymorphismTest/Program.cs
@@ -7,24 +7,49 @@ using System.Threading.Tasks;
 namespace PolymorphismTest
 {
 
-   public class Employee
+    public class Employee
     {
-        public void CalculateWeeklySalary(int hours, int wage)
+        public virtual void CalculateWeeklySalary(int hours, int wage)
         {
             var salary = 40 * wage;
             Console.WriteLine("This employee is angry because he worked for {0} hrs." +
-                "But got paid for 40 hrs at {1}/hrs={2} salary.", hours, wage, salary);
+                "But got paid for 40 hrs at {1}/hrs=$ {2} salary.", hours, wage, salary);
             Console.WriteLine("----------");
 
         }
     }
-    public class Contractor: 
+    public class Contractor : Employee
+    {
+        public override void CalculateWeeklySalary(int hours, int wage)
+        {
+            var salary = hours * wage;
+            Console.WriteLine("\nThis HAPPY CONTRACTOR worked {0} hrs. " +
+                              "Paid for {0} hrs at $ {1}" +
+                              "/hr = ${2} ", hours, wage, salary);
+            Console.WriteLine("---------------------------------------------\n");
+        }
+    }
 
 
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            const int hr = 55, wage = 70;
+
+            List<Employee> listEmployees = GetEmployees();
+            foreach (var e in listEmployees)
+                e.CalculateWeeklySalary(hr, wage);
+            Console.ReadKey();
         }
+        private static List<Employee> GetEmployees()
+        {
+            var emp = new Employee();
+            var cont = new Contractor();
+            var everyone = new List<Employee> { emp, cont };
+            return everyone;
+        }
+       
     }
+
 }
