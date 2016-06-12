1e0b3eec26e7d1c673efadeacda78a0e5426c827
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-06-11 23:26:52 -0700

    Update Program.cs

--- DelegateExample/DelegateExample/Program.cs
+++ DelegateExample/DelegateExample/Program.cs
@@ -1,3 +1,6 @@
+//this is based on the C# delegate, events and lambda tutorial from lynda.com referenced from: http://www.lynda.com/
+
+
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
