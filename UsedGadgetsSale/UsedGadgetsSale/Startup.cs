202b55d516c8ffd9bafb0ef7c0f3c433d3f8fdaf
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-07-11 22:52:29 -0700

    Asp.net website for sale of used gadgets

---
+++ UsedGadgetsSale/UsedGadgetsSale/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UsedGadgetsSale.Startup))]
+namespace UsedGadgetsSale
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
