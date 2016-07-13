54f7c5989b4158d78b9271db776a5447aca3a5d8
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-07-12 21:17:18 -0700

    adding members to Gadget class

--- UsedGadgetsSale/UsedGadgetsSale/Models/Gadget.cs
+++ UsedGadgetsSale/UsedGadgetsSale/Models/Gadget.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using 
+using System.ComponentModel.DataAnnotations;
 
 namespace UsedGadgetsSale.Models
 {
     public class Gadget
     {
+        [ScaffoldColumn(false)]
+        public int GadgetID { get; set; }
+
+        [Required, StringLength(100), Display(Name ="Name")]
+        public string GadgetName { get; set; }
+
+        [Required, StringLength(100), Display(Name ="Gadet Description"), DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+        
+
     }
 }
