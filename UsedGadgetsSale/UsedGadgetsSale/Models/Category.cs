8c87596930211dc4b1f0f2ddd7f05405df0552c3
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-07-13 21:28:56 -0700

    comments added

--- UsedGadgetsSale/UsedGadgetsSale/Models/Category.cs
+++ UsedGadgetsSale/UsedGadgetsSale/Models/Category.cs
@@ -8,7 +8,7 @@ using System.ComponentModel.DataAnnotations;
 namespace UsedGadgetsSale.Models
 {
     public class Category
-    {
+    { //data annotations
         [ScaffoldColumn(false)]
         public int CategoryID { get; set; }
 
