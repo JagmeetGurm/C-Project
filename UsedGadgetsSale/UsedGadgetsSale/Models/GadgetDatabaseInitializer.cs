d4c5ed22b33847c5d3202115c7e8582c3bb42408
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-07-16 23:59:50 -0700

    category items added

--- UsedGadgetsSale/UsedGadgetsSale/Models/GadgetDatabaseInitializer.cs
+++ UsedGadgetsSale/UsedGadgetsSale/Models/GadgetDatabaseInitializer.cs
@@ -118,81 +118,8 @@ namespace UsedGadgetsSale.Models
                     ImagePath="planepaper.png",
                     UnitPrice = 2.95,
                     CategoryID = 2
-                },
-                new Gadget
-                {
-                    GadgetID = 9,
-                    GadgetName = "Propeller Plane",
-                    Description = "Rubber band powered plane features two wheels.",
-                    ImagePath="planeprop.png",
-                    UnitPrice = 32.95,
-                    CategoryID = 2
-                },
-                new Gadget
-                {
-                    GadgetID = 10,
-                    GadgetName = "Early Truck",
-                    Description = "This toy truck has a real gas powered engine. Requires regular tune ups.",
-                    ImagePath="truckearly.png",
-                    UnitPrice = 15.00,
-                    CategoryID = 3
-                },
-                new Gadget
-                {
-                    GadgetID = 11,
-                    GadgetName = "Fire Truck",
-                    Description = "You will have endless fun with this one quarter sized fire truck.",
-                    ImagePath="truckfire.png",
-                    UnitPrice = 26.00,
-                    CategoryID = 3
-                },
-                new Gadget
-                {
-                    GadgetID = 12,
-                    GadgetName = "Big Truck",
-                    Description = "This fun toy truck can be used to tow other trucks that are not as big.",
-                    ImagePath="truckbig.png",
-                    UnitPrice = 29.00,
-                    CategoryID = 3
-                },
-                new Gadget
-                {
-                    GadgetID = 13,
-                    GadgetName = "Big Ship",
-                    Description = "Is it a boat or a ship. Let this floating vehicle decide by using its " +
-                                  "artifically intelligent computer brain!",
-                    ImagePath="boatbig.png",
-                    UnitPrice = 95.00,
-                    CategoryID = 4
-                },
-                new Gadget
-                {
-                    GadgetID = 14,
-                    GadgetName = "Paper Boat",
-                    Description = "Floating fun for all! This toy boat can be assembled in seconds. Floats for minutes!" +
-                                  "Some folding required.",
-                    ImagePath="boatpaper.png",
-                    UnitPrice = 4.95,
-                    CategoryID = 4
-                },
-                new Gadget
-                {
-                    GadgetID = 15,
-                    GadgetName = "Sail Boat",
-                    Description = "Put this fun toy sail boat in the water and let it go!",
-                    ImagePath="boatsail.png",
-                    UnitPrice = 42.95,
-                    CategoryID = 4
-                },
-                new Gadget
-                {
-                    GadgetID = 16,
-                    GadgetName = "Rocket",
-                    Description = "This fun rocket will travel up to a height of 200 feet.",
-                    ImagePath="rocket.png",
-                    UnitPrice = 122.95,
-                    CategoryID = 5
                 }
+                
             };
 
             return Gadgets;
