9b6e94b221c849c6458e3e52325f25c57bc3356d
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-07-21 14:49:20 -0700

    shopping cart class created

--- UsedGadgetsSale/UsedGadgetsSale/ShoppingCart.aspx.cs
+++ UsedGadgetsSale/UsedGadgetsSale/ShoppingCart.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using UsedGadgetsSale.Models;
+using UsedGadgetsSale.Logic;
 
 namespace UsedGadgetsSale
 {
@@ -12,6 +14,12 @@ namespace UsedGadgetsSale
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public List<CartItem> GetShoppingCartItems()
+        {
+            ShoppingCartActions actions = new ShoppingCartActions();
+            return actions.GetCartItems();
         }
     }
 }
