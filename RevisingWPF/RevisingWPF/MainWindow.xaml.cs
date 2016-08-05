f3297ef64101797775a1e14540780bcbfceebcc5
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-08-04 23:45:51 -0700

    input validated

--- RevisingWPF/RevisingWPF/MainWindow.xaml.cs
+++ RevisingWPF/RevisingWPF/MainWindow.xaml.cs
@@ -27,50 +27,78 @@ namespace RevisingWPF
         {
             InitializeComponent();
         }
-
-  /*      private void myMouseUpEvent(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("you clicked me at " + e.GetPosition(this).ToString());
-        }
-*/
-        
-
-        
-
-   /*     private void allFeatures_changed(object sender, RoutedEventArgs e)
-        {
-            CheckBox enableAll = (CheckBox)sender;
-            if (enableAll.IsChecked==true)
-            {
-                firstOp.IsChecked = true;
-                secondOp.IsChecked = true;
-                thirdOp.IsChecked = true;
-
+        private void ValidateName()
+       {
+            if ((txtCompanyName.Text!="") || (txtFirstName.Text !="") && (txtLastName.Text!=""))
+            {labelCompanyName.Content="Company Name";
+                labelFirstName.Content = "First Name";
+                labelLastName.Content = "Last Name";
             }
-            if (enableAll.IsChecked == false)
+            else
             {
-                firstOp.IsChecked = false;
-                secondOp.IsChecked = false;
-                thirdOp.IsChecked = false;
-
+                labelCompanyName.Content = "Company Name*";
+                labelFirstName.Content = "First Name*";
+                labelLastName.Content = "Last Name*";
             }
         }
-
-        private void singlecheck(object sender, RoutedEventArgs e)
+        private void txtFirstName_TextChanged(object sender, TextChangedEventArgs e)
         {
-            allFeatures.IsChecked = null;
-            if(firstOp.IsChecked==true && secondOp.IsChecked==true && thirdOp.IsChecked == true)
-            {
-                allFeatures.IsChecked = true;
-            }
-            if (firstOp.IsChecked == false && secondOp.IsChecked == false && thirdOp.IsChecked == false)
-            {
-                allFeatures.IsChecked = false;
-            }
+            ValidateName();
         }
 
+        private void txtLastName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ValidateName();
+        }
 
-    */
+        private void txtCompanyName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ValidateName();
+        }
+
+        /*      private void myMouseUpEvent(object sender, MouseButtonEventArgs e)
+              {
+                  MessageBox.Show("you clicked me at " + e.GetPosition(this).ToString());
+              }
+      */
+
+
+
+
+        /*     private void allFeatures_changed(object sender, RoutedEventArgs e)
+             {
+                 CheckBox enableAll = (CheckBox)sender;
+                 if (enableAll.IsChecked==true)
+                 {
+                     firstOp.IsChecked = true;
+                     secondOp.IsChecked = true;
+                     thirdOp.IsChecked = true;
+
+                 }
+                 if (enableAll.IsChecked == false)
+                 {
+                     firstOp.IsChecked = false;
+                     secondOp.IsChecked = false;
+                     thirdOp.IsChecked = false;
+
+                 }
+             }
+
+             private void singlecheck(object sender, RoutedEventArgs e)
+             {
+                 allFeatures.IsChecked = null;
+                 if(firstOp.IsChecked==true && secondOp.IsChecked==true && thirdOp.IsChecked == true)
+                 {
+                     allFeatures.IsChecked = true;
+                 }
+                 if (firstOp.IsChecked == false && secondOp.IsChecked == false && thirdOp.IsChecked == false)
+                 {
+                     allFeatures.IsChecked = false;
+                 }
+             }
+
+
+         */
 
         //global exception handling
         // tempString.Trim();
