0d1afe81f31a8c3daab8476c26c2b8b420f103ad
JagmeetGurm <jagmeetgurm@yahoo.com>
2016-08-08 21:25:11 -0700

    add delegate class

--- RevisingWPF/RevisingWPF/MainWindow.xaml.cs
+++ RevisingWPF/RevisingWPF/MainWindow.xaml.cs
@@ -26,6 +26,7 @@ namespace RevisingWPF
         public MainWindow()
         {
             InitializeComponent();
+
         }
         private void ValidateName()
        {
---
+++ RevisingWPF/RevisingWPF/delegateClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisingWPF
+{
+  public static  class delegateClass
+    {
+        //delegate is a reference to a method.
+        public delegate int Del(int firstNo, int secondNo, string operand);
+public static int checkOperand(string operand)
+        {
+
+        }
+        //create a method for the delegate
+        public static int calculateResult(int firstNo, int secondNo, string operand)
+        {
+            int result = 0;
+         result=   checkOperand(operand);
+            return result;
+        }
+    }
+}
